94c6ae82a6ff5564e5d79ddf417b37489a868829
weisti <weiss.oke.till@gmail.com>
2024-12-09 09:32:37 +0100

    GeburtstagsServer

---
+++ src/GeburtstagsServer.java
@@ -0,0 +1,44 @@
+package src;
+import utils.Server;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class GeburtstagsServer extends Server {
+    public GeburtstagsServer() {
+        super(7);
+        System.out.println("Server gestartet");
+    }
+
+    public  void processNewConnection(String pClientIP, int pClientPort) {
+    }
+
+    private Date processDate(String pDate)
+        throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = sdf.parse(pDate);
+        return date;
+    }
+
+    private Date getCurrentDate() throws ParseException {
+        LocalDateTime myDateObj = LocalDateTime.now();
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd/MM/yyyy ");
+        String formattedDate = myDateObj.format(myFormatObj);
+        return processDate(formattedDate);
+    }
+
+    public void processMessage(String pClientIP, int pClientPort, String pMessage) throws ParseException {
+        var diff = TimeUnit.DAYS.convert(Math.abs(processDate(pMessage).getTime() - getCurrentDate().getTime()), TimeUnit.MILLISECONDS);
+        send(pClientIP, pClientPort, String.valueOf(diff));
+    }
+    public void processClosingConnection(String pClientIP, int pClientPort) {
+    }
+
+    public static void main(String[] args){
+        GeburtstagsServer geburtstagsServer = new GeburtstagsServer();
+    }
+}
--- src/QOTDServer.java
+++ src/QOTDServer.java
@@ -1,20 +1,29 @@
+package src;
 import utils.Server;
-public class EchoServer extends Server {
-    public EchoServer() {
+
+public class QOTDServer extends Server {
+    private String[][] arr;
+    public QOTDServer() {
         super(7);
         System.out.println("Server gestartet");
+        arr = new String[1][1];
+        arr[0][0] = "Hallo";
+        arr[0][1] = "-Niklas";
+        arr[1][0] = "Warum";
+        arr[1][1] = "-Berdan";
     }
 
     public  void processNewConnection(String pClientIP, int pClientPort) {
     }
 
     public void processMessage(String pClientIP, int pClientPort, String pMessage) {
+
         send(pClientIP, pClientPort, pMessage);
     }
     public void processClosingConnection(String pClientIP, int pClientPort) {
     }
 
     public static void main(String[] args){
-        EchoServer echoServer = new EchoServer();
+        QOTDServer qotdServer = new QOTDServer();
     }
-} 
+}
